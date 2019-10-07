fae956b684a6eb3ffa2bd9bfaf36479bec044d1f
Robert Holland <robertrhollandiii@gmail.com>
2019-10-07 14:42:25 -0400

    Working on method for "," and "\n"

--- 16_Test_Driven_Development/pair-exercise/Exercises.Test/StringCalcTest.cs
+++ 16_Test_Driven_Development/pair-exercise/Exercises.Test/StringCalcTest.cs
@@ -10,6 +10,8 @@ namespace Exercises.Test
         [DataTestMethod]
         [DataRow("", 0)] //First test will test for and empty string and return "0"
         [DataRow("1", 1)]//If only a number is provided in the string, then the number should be returned.
+        [DataRow("1,2", 3)]//If only a number is provided in the string, then the number should be returned.
+        [DataRow("5\n3,2", 10)]
         public void EmptyString(string stringToTest, int expectedResult)
         {
             //Arrange
--- 16_Test_Driven_Development/pair-exercise/Exercises/StringCalculatorKata.cs
+++ 16_Test_Driven_Development/pair-exercise/Exercises/StringCalculatorKata.cs
@@ -6,20 +6,35 @@ namespace Exercises
 {
     public class StringCalculatorKata
     {
+        
         public int EmptyString(string value)
         {
-            if (value == "")
+            if (value == "")//First test will test for and empty string and return "0"
             {
                 return 0;
             }
+            else if (value.Length == 1) //If only a number is provided in the string, then the number should be returned.
+            {
+                return int.Parse(value);
+            }
             else
             {
-                return 1;
+                string[] valueString = new string[value.Length];
+                value.Split(new Char[] { ',', '\n' });
+
+                int i = 0;
+                foreach (string word in valueString)
+                {
+                    i = int.Parse(word);
+                }
+
+                int sum = i;
+                for (int n = 1; n < value.Length; n++)
+                {
+                    sum += value[n];
+                }
+                return sum;
             }
         }
-    }
-
-    public class KataFizzBuzz : StringCalculatorKata
-    {
     }
 }
