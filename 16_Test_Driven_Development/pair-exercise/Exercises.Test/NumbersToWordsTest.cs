1a436ccc127b25911f69325844d765dd29903068
Katherine Kimes <kbk15@case.edu>
2019-10-07 15:07:57 -0400

    Commit Test for //Testing for converting int 1 to string "one"

---
+++ 16_Test_Driven_Development/pair-exercise/Exercises.Test/NumbersToWordsTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises.Test
+{
+     [TestClass]
+     public class NumbersToWordsTest
+    {
+    
+        [DataTestMethod]
+        [DataRow(1, "one")] //Testing for converting int 1 to string "one"
+
+        public void NumberString(int numberToTest, string expectedResult)
+        {
+            //Arrange
+            // Create a NumberToWords Object
+            NumbersToWords number = new NumbersToWords();
+
+            //Act
+            string actualResult = number.NumberString(numberToTest);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
---
+++ 16_Test_Driven_Development/pair-exercise/Exercises/NumbersToWords.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises
+{
+    public class NumbersToWords
+    {
+        public string NumberString(int numberToTest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
